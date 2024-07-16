a7ec2f45bdef757d9a1080edfe86a375b3976b80
CharanjeetI2D <charanjeet8149@gmail.com>
2024-07-16 19:38:27 +0530

    Initial Commit

--- web.py
+++ web.py
@@ -3,11 +3,15 @@ import functions
 
 todos = functions.get_todos()
 
+
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
 
+
+st.text_input(label=" ", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
 
 st.title("My Todo App")
 st.subheader("What's on your mind today?")
@@ -20,6 +24,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label=" ", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
